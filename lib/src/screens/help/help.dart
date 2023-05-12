fa6e57f7788623d0e474df20648b0e6e8d6677cc
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-12 04:09:22 +0300

    fixed help page

--- lib/src/screens/help/help.dart
+++ lib/src/screens/help/help.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:jiko_langu/src/utils/app_const.dart';
 import 'package:jiko_langu/src/widgets/app_base_screen.dart';
+import 'package:jiko_langu/src/widgets/app_text.dart';
 
 class help extends StatefulWidget {
   const help({Key? key}) : super(key: key);
@@ -11,6 +13,21 @@ class help extends StatefulWidget {
 class _helpState extends State<help> {
   @override
   Widget build(BuildContext context) {
-    return AppBaseScreen(child: Container());
+    return AppBaseScreen(
+        bgcolor: AppConst.black,
+        child:  Column(
+          children: [
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                Icon(Icons.arrow_back, color: AppConst.primary,),
+                AppText(txt: 'Help', size: 15, color: AppConst.primary,),
+                Spacer(),
+                AppText(txt: 'ID 2300998', size: 15, color: AppConst.primary,)
+              ],
+            ),
+          ],
+        )
+    );;
   }
 }
