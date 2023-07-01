8000ca65f2483be563417d86dd6a6b08c7581478
Kwayu Mmari <mmaridenis@gmail.com>
2023-07-01 15:09:51 +0300

    fixed

--- lib/src/screens/home/homepage.dart
+++ lib/src/screens/home/homepage.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:jiko_langu/routes/route-names.dart';
-import 'package:jiko_langu/src/service/data.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:jiko_langu/src/utils/app_const.dart';
 import 'package:jiko_langu/src/widgets/app_base_screen.dart';
-import 'package:jiko_langu/src/widgets/app_button.dart';
-import 'package:jiko_langu/src/widgets/app_dropdown.dart';
-import 'package:jiko_langu/src/widgets/app_input_text.dart';
-import 'package:jiko_langu/src/widgets/app_snackbar.dart';
 import 'package:jiko_langu/src/widgets/app_text.dart';
 import 'package:jiko_langu/src/widgets/app_toggle_button.dart';
 
 class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+  const Homepage({Key? key});
 
   @override
   State<Homepage> createState() => _HomepageState();
@@ -19,51 +14,84 @@ class Homepage extends StatefulWidget {
 
 class _HomepageState extends State<Homepage> {
   bool value = false;
+  FlutterBluetoothSerial bluetooth = FlutterBluetoothSerial.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    bluetooth.state.then((state) {
+      setState(() {
+        value = (state == BluetoothState.STATE_ON);
+      });
+    });
+  }
+
+  void _toggleBluetooth(bool newValue) async {
+    if (newValue) {
+      await bluetooth.requestEnable();
+    } else {
+      await bluetooth.requestDisable();
+    }
+    setState(() {
+      value = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return AppBaseScreen(
-        bgcolor: AppConst.black,
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: AppConst.primary,
-          centerTitle: true,
-          title: AppText(
-            txt: 'Welcome back John Doe',
-            size: 15,
-            color: AppConst.secondary,
-          ),
+      bgcolor: AppConst.black,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: AppConst.primary,
+        centerTitle: true,
+        title: AppText(
+          txt: 'Welcome back John Doe',
+          size: 15,
+          color: AppConst.secondary,
         ),
-        child: Column(
-          children: [
-            SizedBox(height: 50,),
-            Container(
-              width: 350,
-              height: 100,
-              decoration: BoxDecoration(
-                color: AppConst.primary,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(20.0),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: AppText(txt: 'Welcome to JIKOLANGU! Control your stove at your pace and prepare your food freely as you wish', size: 20, color: AppConst.secondary,),
+      ),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            width: 350,
+            height: 100,
+            decoration: BoxDecoration(
+              color: AppConst.primary,
+              borderRadius: BorderRadius.all(
+                Radius.circular(20.0),
               ),
             ),
-            SizedBox(height: 30,),
-            CircleAvatar(
-              radius: 100,
-              backgroundImage: AssetImage('assets/home.jpg'),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AppText(
+                txt:
+                    'Welcome to JIKOLANGU! Control your stove at your pace and prepare your food freely as you wish',
+                size: 20,
+                color: AppConst.secondary,
+              ),
             ),
-            SizedBox(height: 50,),
-            AppToggleButton(title: 'Bluetooth', value: value, onChanged: (newValue) {
-              setState(() {
-                value = !value;
-              });
-            })
-
-          ],
-        ));
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          CircleAvatar(
+            radius: 100,
+            backgroundImage: AssetImage('assets/home.jpg'),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          AppToggleButton(
+            title: 'Bluetooth',
+            value: value,
+            onChanged: _toggleBluetooth,
+          ),
+        ],
+      ),
+    );
   }
 }
